4e670176f6fcbab1b489409c8f6a1d00b5c6d544
Ujjwal Pratap Singh <a-9609@kmclu.ac.in>
2026-07-31 21:46:15 +0530

    Create provider.tf

---
+++ 04_ec2_with_variable/provider.tf
@@ -0,0 +1,14 @@
+#Downloading aws provider
+
+terraform {
+	required_providers {
+		aws= {
+			source= "hashicorp/aws"
+			version= "5.91.0"
+		}
+	}	
+}
+
+provider "aws" {
+	region= "us-east-1"
+}
